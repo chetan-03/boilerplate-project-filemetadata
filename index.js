d81d53ca50b9af7d0a6c044acff8e4bd3e2687ce
Chetan Vanam <chetanvanam02@gmail.com>
2023-12-30 19:46:41 +0530

    final commit by chetan vanam

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
-
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+const upload = multer({ storage })
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // get the uploaded file
+  const file = req.file
+  console.log('log from post request', req.body, req.files, req.file);
+  // get the file size
+  const size = file.size
+
+
+  // get the file type
+
+  const type = file.mimetype
+
+  // get the file name
+
+  const name = file.originalname
+
+
+  return res.json({
+    name, type, size
+  })
+})
 
 
 
---
+++ uploads/1703945646077-app.js
@@ -0,0 +1,62 @@
+// index.js
+// where your node app starts
+
+// for env variables
+
+require('dotenv').config()
+// init project
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/", (req, res) => {
+
+  return res.json({
+    unix: Date.now(),
+    utc: new Date().toUTCString()
+  })
+
+})
+
+app.get("/api/:date", (req, res) => {
+  if (isNaN(req.params.date)) {
+
+    var date = new Date(req.params.date)
+
+  } else {
+
+    var date = new Date(Number(req.params.date))
+
+  }
+  if (date == 'Invalid Date') return res.json({ error: 'Invalid Date' })
+  console.log(date, req.params.date)
+  const utc = date.toUTCString()
+  const unix = Date.parse(utc)
+  return res.json({ unix, utc })
+})
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+console.log(process.env.myname)
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
